59558cbeb37ee3fc80db8bbec7bd40cbe5103c1e
aperezb97 <aperezbrugiafreddo@gmail.com>
2022-07-26 21:48:34 -0300

    primer avance

--- 1-Data Preprocessing/Proyecto_1.py
+++ 1-Data Preprocessing/Proyecto_1.py
@@ -3,3 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('Data.csv')
+
+# x es la variable independiente y la y la dependeinte que sera comprado.
+x = df.iloc[:, :-1].values #todas las columnas menos las ultimas
+y = df.iloc[:, 3].values #solo la tercer columna
+print(x, y)
+
