eb029caf04312d403b1ba501a9a943f7de6d11c4
aperezb97 <aperezbrugiafreddo@gmail.com>
2022-07-30 22:27:39 -0300

    import sklearn

--- 1-Data Preprocessing/Proyecto_1.py
+++ 1-Data Preprocessing/Proyecto_1.py
@@ -1,14 +1,16 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 df = pd.read_csv('Data.csv')
-
+df.head()
 # x es la variable independiente y la y la dependeinte que sera comprado.
 x = df.iloc[:, :-1].values #todas las columnas menos las ultimas
 y = df.iloc[:, 3].values #solo la tercer columna
-print(df)
+
+
 
 #solucionar datos NaN
 
 
+ 
